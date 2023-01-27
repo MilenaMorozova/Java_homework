704563a9c35b2fe55cdba3759dab270f030b2330
MilenaMorozova <50710003+MilenaMorozova@users.noreply.github.com>
2023-01-27 11:11:42 +0300

    Delete homework4 directory

--- homework4/Main.java
+++
@@ -1,7 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        Frequency frequency = new Frequency("input.txt", "frequency.txt");
-        frequency.readFile();
-        frequency.writeToFile();
-    }
-}
